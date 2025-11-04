2dc1d10e79d8feaceb55a3c23c47fbf9f6a64c59
Ubuntu <ubuntu@ip-172-31-45-150.ap-south-1.compute.internal>
2025-11-04 09:10:31 +0000

    changes in config file

--- dist/src/__tests__/customer.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-describe('Dummy test', () => {
-    it('should pass', () => {
-        expect(true).toBe(true);
-    });
-});
