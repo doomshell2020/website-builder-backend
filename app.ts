77bfea72b7b5b0ab975f6d21d89d27f883fbab43
rahuldevgit-hub <choudharyrahul7600@gmail.com>
2025-11-05 13:50:25 +0530

    05-11-2025

--- app.ts
+++ app.ts
@@ -9,21 +9,23 @@ import { errorHandler, badJsonHandler } from './src/middleware/index';
 require('dotenv').config({ path: '.env.local' });
 const app = express();
 // ---------------------- Middleware ---------------------- //
-// app.use(cors());
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   "http://192.168.0.77:3000",
-//   "http://192.168.0.77:5000",
-//   "http://localhost:5002",
-//   "http://webbuilder.local:3000",
-//   "https://navlok.doomshell.com",
-//   "https://navvistarinfra.doomshell.com",
-//   process.env.SITE_URL,
-// ];
-app.use(cors({
-  origin: "*",
-  credentials: true,
-}));
+app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://192.168.0.77:3000",
+  "http://192.168.0.77:5000",
+  "http://localhost:5002",
+  "http://webbuilder.local:3000",
+  "https://navlok.doomshell.com",
+  "https://navvistarinfra.doomshell.com",
+  "https://website-builder-frontend-three.vercel.app",
+  "https://api.doomshell.com",
+  process.env.SITE_URL,
+];
+// app.use(cors({
+//   origin: "*",
+//   credentials: true,
+// }));
 
 // 🧭 Determine the correct upload path based on environment
 const UPLOAD_DIR =
@@ -33,28 +35,29 @@ const UPLOAD_DIR =
 
 app.use('/uploads', express.static(UPLOAD_DIR));
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true); // Allow server-to-server or Postman requests
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); // Allow server-to-server or Postman requests
 
-//       // ✅ Allow fixed origins
-//       if (allowedOrigins.includes(origin)) {
-//         return callback(null, true);
-//       }
+      // ✅ Allow fixed origins
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
 
-//       // ✅ Allow any subdomain of webbuilder.local:3000
-//       const subdomainRegex = /^http:\/\/([a-zA-Z0-9-]+)\.webbuilder\.local:3000$/;
-//       if (subdomainRegex.test(origin)) {
-//         return callback(null, true);
-//       }
+      // ✅ Allow any subdomain of webbuilder.local:3000
+      // const subdomainRegex = /^http:\/\/([a-zA-Z0-9-]+)\.webbuilder\.local:3000$/;
+      const subdomainRegex = /^https?:\/\/([a-zA-Z0-9-]+)\.doomshell\.com$/;
+      if (subdomainRegex.test(origin)) {
+        return callback(null, true);
+      }
 
-//       // ❌ Otherwise, block it
-//       return callback(new Error("Not allowed by CORS: " + origin));
-//     },
-//     credentials: true,
-//   })
-// );
+      // ❌ Otherwise, block it
+      return callback(new Error("Not allowed by CORS: " + origin));
+    },
+    credentials: true,
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
