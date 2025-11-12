fe40e34d32fea22add6835f368549406f584b50c
rahuldevgit-hub <choudharyrahul7600@gmail.com>
2025-11-12 10:48:28 +0530

    12-11-2025

--- src/services/admin/profile.service.ts
+++ src/services/admin/profile.service.ts
@@ -4,6 +4,6 @@ const { User } = db;
 // Find user by ID
 export const findUserById = async (id: string | number) => {
   return await User.findByPk(id, {
-    attributes: { exclude: ['password', 'createdAt', 'updatedAt', 'deleted','role', 'company_logo'] },
+    attributes: { exclude: ['password', 'createdAt', 'updatedAt', 'deleted','role'] },
   });
 };
